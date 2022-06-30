5303830799fb3a1c1aba4971c422ec04bc2c0084
AHMAD ZAKWAN AMIR BIN MOHD <zakwan.mohd@redone.com.my>
2022-06-30 17:15:23 +0800

    ammend some codes

--- lib/posts_comments.dart
+++ lib/posts_comments.dart
@@ -18,7 +18,6 @@ class PostsComments extends StatefulWidget {
   State<PostsComments> createState() => _PostsCommentsState();
 }
 
-
 class _PostsCommentsState extends State<PostsComments> {
   // late Timer debouncer;
   final postsCommentsModel = PostsCommentsModel();
@@ -44,18 +43,18 @@ class _PostsCommentsState extends State<PostsComments> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Posts Comments'),
-      ),
-      body: 
-      // FutureBuilder<List<PostsCommentsModel>>(
-      //   future: postsCommentsLists,
-      //   builder: (context, snapshot) {
-      //     if (snapshot.hasData) {
-      //       List<PostsCommentsModel> data = snapshot.data!;
-      //       return 
+        appBar: AppBar(
+          title: const Text('Posts Comments'),
+        ),
+        body:
+            // FutureBuilder<List<PostsCommentsModel>>(
+            //   future: postsCommentsLists,
+            //   builder: (context, snapshot) {
+            //     if (snapshot.hasData) {
+            //       List<PostsCommentsModel> data = snapshot.data!;
+            //       return
             Column(children: [
-              TextField(
+          TextField(
             decoration: const InputDecoration(
               contentPadding: EdgeInsets.all(15.0),
               hintText: 'Filter by name or email',
@@ -67,75 +66,79 @@ class _PostsCommentsState extends State<PostsComments> {
                       .where((u) => (u.name!
                               .toLowerCase()
                               .contains(string.toLowerCase()) ||
-                          u.email!.toLowerCase().contains(string.toLowerCase())))
+                          u.email!
+                              .toLowerCase()
+                              .contains(string.toLowerCase())))
                       .toList();
                 });
               });
             },
           ),
-              // SizedBox(
-              //   // width: 100,
-              //   child: SearchFilterWidget(
-              //       text: query,
-              //       onChanged: searchFilter,
-              //       hintText: 'Name, email or comments'),
-              // ),
-              Expanded(
-                  child: ListView.builder(
-                      itemCount: filteredComments.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        // final comment = data[index];
-                        // return buildComments(comment);
-                        return Padding(
-                            padding: const EdgeInsets.only(
-                                top: 10, left: 5, right: 5),
-                            child: Container(
-                                padding: const EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.grey),
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(10)),
-                                ),
-                                child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        right: 8.0,
-                                        left: 8.0,
-                                        top: 10.0,
-                                        bottom: 10.0),
-                                    child: ExpansionTile(
-                                      title: IntrinsicHeight(
-                                        child: Row(
-                                          children: [
-                                            Expanded(
-                                              flex: 72,
-                                              child: Column(
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.start,
-                                                children: [
-                                                  Text(filteredComments[index].name ?? '',
-                                                      maxLines: 1),
-                                                  Text(filteredComments[index].email ?? '',
-                                                      maxLines: 1),
-                                                ],
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: filteredComments.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    // final comment = data[index];
+                    // return buildComments(comment);
+                    return Padding(
+                        padding:
+                            const EdgeInsets.only(top: 10, left: 5, right: 5),
+                        child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                            ),
+                            child: Padding(
+                                padding: const EdgeInsets.only(
+                                    right: 8.0,
+                                    left: 8.0,
+                                    top: 10.0,
+                                    bottom: 10.0),
+                                child: ExpansionTile(
+                                  title: IntrinsicHeight(
+                                    child: Row(
                                       children: [
-                                        Text(filteredComments[index].body ?? '',
-                                            maxLines: 7)
+                                        Expanded(
+                                          flex: 72,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                filteredComments[index].name ??
+                                                    '',
+                                                maxLines: 1,
+                                                style: const TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              Text(
+                                                  filteredComments[index]
+                                                          .email ??
+                                                      '',
+                                                  maxLines: 1),
+                                            ],
+                                          ),
+                                        ),
                                       ],
-                                    ))));
-                      }))
-            ])
-      //     } else if (snapshot.hasError) {
-      //       return Text("${snapshot.error}");
-      //     }
-      //     // By default show a loading spinner.
-      //     return const CircularProgressIndicator();
-      //   },
-      // ),
-    );
+                                    ),
+                                  ),
+                                  children: [
+                                    Text(filteredComments[index].body ?? '',
+                                        maxLines: 7)
+                                  ],
+                                ))));
+                  }))
+        ])
+        //     } else if (snapshot.hasError) {
+        //       return Text("${snapshot.error}");
+        //     }
+        //     // By default show a loading spinner.
+        //     return const CircularProgressIndicator();
+        //   },
+        // ),
+        );
   }
 }
